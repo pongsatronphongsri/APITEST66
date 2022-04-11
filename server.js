f3bd6e0464305bc370b3b8dda7ca06d84c0af72f
pongsatronphongsri <6306021622011@fitm.kmutnb.ac.th>
2022-04-11 16:53:30 +0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 require('dotenv').config()
-const expressLayouts = require('express-ejs-layouts')
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const {
+    checkAuthenticated,
+    checkNotAuthenticated,
+  } = require("./middlewares/auth");
+
+const app = express()
+ const initializePassport = require("./passport-config");
+  initializePassport(
+    passport,
+    async (email) => {
+      const userFound = await User.findOne({ email });
+      return userFound;
+    },
+    async (id) => {
+      const userFound = await User.findOne({ _id: id });
+      return userFound;
+    }
+  ); 
+  
+
+
+
+
 
 mongoose.connect(process.env.DATABASE_URL)
+
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open',() => console.log('Connected to Database'))
@@ -13,9 +41,77 @@ db.once('open',() => console.log('Connected to Database'))
 app.set("view engine" ,"ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
-app.get("/",(req,res)=>{
-    res.render("index");
+app.use(session({ secret: 'somevalue' }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+
+
+
+
+
+app.get("/",checkAuthenticated,(req,res)=>{
+    res.render("index",{name:req.user.name});
 });
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+    res.render("register");
+  });
+  
+app.get("/login", checkNotAuthenticated, (req, res) => {
+    res.render("login");
+});
+
+app.post(
+    "/login",
+    checkNotAuthenticated,
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true,
+    })
+  );
+
+  app.post("/register", checkNotAuthenticated, async (req, res) => {
+    const userFound = await User.findOne({ email: req.body.email });
+  
+    if (userFound) {
+      req.flash("error", "User with that email already exists");
+      res.redirect("/register");
+    } else {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+        });
+  
+        await user.save();
+        res.redirect("/login");
+      } catch (error) {
+        console.log(error);
+        res.redirect("/register");
+      }
+    }
+});
+
+app.delete("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/login");
+  });
+
+
+
 
 
 
